5aa4b8cb68839f18fc5e9d8a995303b9daf0dd8f
Pythonikk <nikkibrusoe@gmail.com>
2022-05-02 20:34:01 -0700

    Fix devise issues with logging out

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   before_action :authenticate_member!, except: [:index]
 
-  def index; end
+  def index
+    @posts = Post.all
+  end
 
   def new; end
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,9 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  # Changed to :get to fix Routing Error: No route matches [GET]'users/sign_out'
+  # Devise isnt accepting 'method: :delete' in my view.
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
